56005b2cccfd097afc2b09a694a033dea5ab5e3f
djibzo <djibzo90@gmail.com>
2024-08-30 21:24:59 +0000

    Creation component pour liste daara

---
+++ frontend/src/app/components/liste-daara/liste-daara.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import "src/assets/plugins/datatable/js/jquery.dataTables.min.js";
+@Component({
+  selector: 'app-liste-daara',
+  templateUrl: './liste-daara.component.html',
+  styleUrls: ['./liste-daara.component.scss']
+})
+export class ListeDaaraComponent {
+
+}
