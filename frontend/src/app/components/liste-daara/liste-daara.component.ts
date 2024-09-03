30fc4cb1bf0c7a3e8feca4ac28d6fd385f51f268
djibzo <djibzo90@gmail.com>
2024-09-03 01:07:51 +0000

    Suite design details daara

--- frontend/src/app/components/liste-daara/liste-daara.component.ts
+++ frontend/src/app/components/liste-daara/liste-daara.component.ts
@@ -11,6 +11,7 @@ export class ListeDaaraComponent implements OnInit {
   ngOnInit(): void {
     this.getDaaras();
     this.createPieChart();
+    this.createPieChart2();
   }
 
   daaras: Daara[] = [];
@@ -39,8 +40,8 @@ export class ListeDaaraComponent implements OnInit {
         data: {
           labels: ['Garcon', 'Fille'],
           datasets: [{
-            label: 'Repartition des sexes',
-            data: [45, 55], // Remplacez ces valeurs par vos données réelles
+            label: 'Nombre',
+            data: [55, 45], // Remplacez ces valeurs par vos données réelles
             backgroundColor: [
               'rgba(54, 162, 235, 0.6)', // Couleur pour garcon
               'rgba(255, 99, 132, 0.6)'  // Couleur pour fille
@@ -58,7 +59,7 @@ export class ListeDaaraComponent implements OnInit {
             legend: {
               position:'top',
             },
-            datalabels: {
+            /*datalabels: {
               display: true,
               formatter: (value: number, context: any) => {
                 const total = context.chart.getDatasetMeta(0).total;
@@ -69,9 +70,10 @@ export class ListeDaaraComponent implements OnInit {
               anchor: 'end',
               align: 'start',
               offset: 10,
-            },
+            },*/
             tooltip: {
-              callbacks: {
+              enabled:true,
+              /*callbacks: {
                 label: function (context) {
                   let label = context.label || '';
                   if (context.parsed) {
@@ -82,7 +84,68 @@ export class ListeDaaraComponent implements OnInit {
                   }
                   return label;
                 }
-              }
+              }*/
+            }
+          }
+        }
+      });
+    }
+  }
+  private createPieChart2(): void {
+    const ctx = document.getElementById('pieChart2') as HTMLCanvasElement;
+    if (ctx) {
+      new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: ['5-7 ans', '7-10 ans', '10-15 ans'],
+          datasets: [{
+            label: 'Nombre',
+            data: [30,32,29], // Remplacez ces valeurs par vos données réelles
+            backgroundColor: [
+              'rgba(120,235,54,0.6)', // Couleur pour garcon
+              'rgb(232,46,68)'  ,// Couleur pour fille
+              'rgb(118,85,250)'  ,// Couleur pour fille
+            ],
+            borderColor: [
+              'rgba(120,235,54,0.6)', // Couleur pour garcon
+              'rgb(232,46,68)'  ,// Couleur pour fille
+              'rgb(118,85,250)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position:'top',
+            },
+            /*datalabels: {
+              display: true,
+              formatter: (value: number, context: any) => {
+                const total = context.chart.getDatasetMeta(0).total;
+                const percentage = (value / total * 100).toFixed(2) + '%';
+                return percentage;
+              },
+              color: '#fff',
+              anchor: 'end',
+              align: 'start',
+              offset: 10,
+            },*/
+            tooltip: {
+              enabled:true,
+              /*callbacks: {
+                label: function (context) {
+                  let label = context.label || '';
+                  if (context.parsed) {
+                    // @ts-ignore
+                    const total = context.chart.getDatasetMeta(0).total;
+                    const percentage = (context.parsed / total * 100).toFixed(2) + '%';
+                    label += `: ${percentage}`;
+                  }
+                  return label;
+                }
+              }*/
             }
           }
         }
