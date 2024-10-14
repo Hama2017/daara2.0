5ea8eb63ad99f48d3eb0eb96a708f7b9c7160748
bassinediallo <bassinediallo20@gmail.com>
2024-10-14 14:50:52 +0000

    Ajout du modèle Niveau et du service DataNiveauService

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -19,8 +19,8 @@ class UserSeeder extends Seeder
             User::create([
                 'nomUser' => "Responsable Daara",
                 'prenomUser' => $i, // Utilise le numéro pour distinguer les utilisateurs
-                'emailUser' => "responsable$i@example.com",
-                'mdpUser' => Hash::make('password'), // Remplacez 'password' par le mot de passe désiré
+                'emailUser' => "bassinediallo20$i@gmail.com",
+                'mdpUser' => Hash::make('passer'), // Remplacez 'password' par le mot de passe désiré
                 'telephoneUser' => "Téléphone $i",
                 'idProfil' => $responsableProfilId,
             ]);
---
+++ frontend/src/app/models/niveau.ts
@@ -0,0 +1,8 @@
+export interface Niveau {
+    idNiveau: number;
+    nomNiveau: string;
+    mensualiteNiveau: number;
+    droitInscriptionNiveau: number;
+    dureeNiveau: number;
+}
+  
